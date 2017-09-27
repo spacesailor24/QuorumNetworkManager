e10b7686b18ddd0c3dc4a7f7f0d55684cd8290eb
rynobey <rynobey@gmail.com>
2017-09-27 23:40:29 +0200

    Moved whisper post JSON object creation to separate file.

--- Communication/messageStrings.js
+++ Communication/messageStrings.js
@@ -4,20 +4,46 @@ function buildString(type, parameter) {
 	return type + delimiter + parameter;
 }
 
-module.exports = {
-  join: function(str1, str2) {
-    return str1 + delimiter + str2;
-  },
-  request: {
-    ether: buildString('request', 'ether'),
-    enode: buildString('request', 'enode'),
-    genesisBlock: buildString('request', 'genesisBlock'),
-    staticNode: buildString('request', 'staticNode')
-  },
-  response: {
-    ether: buildString('response', 'ether'),
-    enode: buildString('response', 'enode'),
-    genesisBlock: buildString('response', 'genesisBlock'),
-    staticNode: buildString('response', 'staticNode')
+function join(str1, str2) {
+  return str1 + delimiter + str2;
+}
+
+function buildPostObject(topics, payload, ttl, workToProve, id) {
+  postObj = { 
+    JSON: {
+      'topics': topics,
+      'payload': payload,
+      'ttl': ttl,
+      'workToProve': workToProve
+    },
+    filterObject: buildFilterObject(topics)
+  };
+  if (id != undefined) {
+    postObj.JSON.from = id
   }
+  return postObj;
+}
+
+function buildFilterObject(topics) {
+  return {'topics': topics};
+}
+
+request = {
+  ether: buildString('request', 'ether'),
+  enode: buildString('request', 'enode'),
+  genesisBlock: buildString('request', 'genesisBlock'),
+  staticNode: buildString('request', 'staticNode')
 };
+
+response = {
+  ether: buildString('response', 'ether'),
+  enode: buildString('response', 'enode'),
+  genesisBlock: buildString('response', 'genesisBlock'),
+  staticNode: buildString('response', 'staticNode')
+};
+
+exports.Join = join;
+exports.BuildPostObject = buildPostObject;
+exports.BuildFilterObject = buildFilterObject;
+exports.Request = request;
+exports.Response = response;
