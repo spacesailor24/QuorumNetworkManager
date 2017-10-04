86fd7c9ae72034a43a195ff9068d2084d5a934ce
Coenie Beyers <coeniebeyers@gmail.com>
2017-10-04 14:43:38 +0200

    Added check for constellation version

--- constellation.js
+++ constellation.js
@@ -1,49 +1,72 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
 
-function createNewConstellationKeys(result, cb){
-  var counter = result.constellationKeySetup.length;
-  var cmd = "";
-  for(var i in result.constellationKeySetup){
-    var folderName = result.constellationKeySetup[i].folderName;
-    var fileName = result.constellationKeySetup[i].fileName;
-    cmd += 'cd '+folderName+' && constellation-node --generatekeys='+fileName+' && cd .. && '; 
-  }
-  cmd = cmd.substring(0, cmd.length-4);
-  var child = exec(cmd);
+function checkVersionOfConstellation(cb){
+  let cmd = 'constellation-node --version'
+  let child = exec(cmd)
   child.stdout.on('data', function(data){
-    if(data.indexOf('Lock key pair') >= 0){
-      child.stdin.write('\n');
-      counter--;
-      if(counter <= 0){
-        cb(null, result);
-      } 
+    if(data.includes('Constellation Node 0.1.0') == false){
+      console.log('Incorrect version of constellation installed, please refer to https://github.com/ConsenSys/QuorumNetworkManager/tree/UpgradingConstellation#upgrading-constellation')
+      cb(false)
     } else {
-      console.log('Unexpected data:', data);
-      cb(null, result);
+      cb(true)
     }
-  });
+  })
   child.stderr.on('data', function(error){
-    console.log('ERROR:', error);
-    cb(error, null);
-  });
+    console.log('constellation-node --version ERROR:', error)
+    cb(false)
+  })
+}
+
+function createNewConstellationKeys(result, cb){
+  checkVersionOfConstellation(function(correctVersion){
+    if(correctVersion === true){
+      var counter = result.constellationKeySetup.length;
+      var cmd = "";
+      for(var i in result.constellationKeySetup){
+        var folderName = result.constellationKeySetup[i].folderName;
+        var fileName = result.constellationKeySetup[i].fileName;
+        cmd += 'cd '+folderName+' && constellation-node --generatekeys='+fileName+' && cd .. && '; 
+      }
+      cmd = cmd.substring(0, cmd.length-4);
+      var child = exec(cmd);
+      child.stdout.on('data', function(data){
+        if(data.indexOf('Lock key pair') >= 0){
+          child.stdin.write('\n');
+          counter--;
+          if(counter <= 0){
+            cb(null, result);
+          } 
+        } else {
+          console.log('Unexpected data:', data);
+          cb(null, result);
+        }
+      });
+      child.stderr.on('data', function(error){
+        console.log('ERROR:', error);
+        cb(error, null);
+      });
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
 function createConstellationConfig(result, cb){
-  var c = result.constellationConfigSetup;
-  var config = 'url = "http://'+c.localIpAddress+':'+c.localPort+'/"\n';
-  config += 'port = '+c.localPort+'\n';
-  config += 'socket = "'+c.folderName+'/socket.ipc"\n';
-  config += 'othernodes = ["http://'+c.remoteIpAddress+':'+c.remotePort+'/"]\n';
+  let c = result.constellationConfigSetup
+  let config = 'url = "http://'+c.localIpAddress+':'+c.localPort+'/"\n'
+  config += 'port = '+c.localPort+'\n'
+  config += 'socket = "'+c.folderName+'/socket.ipc"\n'
+  config += 'othernodes = ["http://'+c.remoteIpAddress+':'+c.remotePort+'/"]\n'
   config += 'publickeys = ["'+c.folderName+'/'+c.publicKeyFileName+
-    '","'+c.folderName+'/'+c.publicArchKeyFileName+'"]\n';
+    '","'+c.folderName+'/'+c.publicArchKeyFileName+'"]\n'
   config += 'privatekeys = ["'+c.folderName+'/'+c.privateKeyFileName+
-    '","'+c.folderName+'/'+c.privateArchKeyFileName+'"]\n';
-  config += 'storage = "'+c.folderName+'/data"';
+    '","'+c.folderName+'/'+c.privateArchKeyFileName+'"]\n'
+  config += 'storage = "'+c.folderName+'/data"'
   fs.writeFile(c.configName, config, function(err, res){
-    cb(err, result);
+    cb(err, result)
   });
 }
 
-exports.CreateNewKeys = createNewConstellationKeys;
-exports.CreateConfig = createConstellationConfig;
+exports.CreateNewKeys = createNewConstellationKeys
+exports.CreateConfig = createConstellationConfig
