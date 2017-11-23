baf3b49690ee0f25ce10cc7f205b5f82370c9f72
Coenie Beyers <coeniebeyers@gmail.com>
2017-11-23 13:39:51 +0200

    Added function for asymmetric key. Added postAtInterval method in whisperUtils. Created new buildPostObject method.

--- Communication/whisperUtils.js
+++ Communication/whisperUtils.js
@@ -1,7 +1,7 @@
 var messageString = require('./messageStrings.js');
 var config = require('../config.js')
 
-function getNetworkBootstrapKey(shh, cb){
+function getSymmetricKey(shh, cb){
   if(config.whisper.symKeyID){
     cb(null, config.whisper.symKeyID)
   } else {
@@ -13,8 +13,19 @@ function getNetworkBootstrapKey(shh, cb){
   }
 }
 
+function getAsymmetricKey(shh, cb){
+  if(config.whisper.asymKeyID){
+    cb(null, config.whisper.asymKeyID)
+  } else {
+    let id = shh.newKeyPair(function(err, id){
+      config.whisper.asymKeyID = id
+      cb(err, config.whisper.asymKeyID)
+    })
+  }
+}
+
 function addSubscription(symKeyID, topicArr, web3RPC, onData){
-  let topics = messageString.BuildFilterObject(topicArr).topics
+  let topics = buildFilterObject(topicArr).topics
   let subscription = web3RPC.shh.subscribe('messages', {topics, symKeyID})
   subscription.on('data', onData)
   subscription.on('error', function(error){
@@ -24,10 +35,56 @@ function addSubscription(symKeyID, topicArr, web3RPC, onData){
 
 function addBootstrapSubscription(topics, web3RPC, onData){
 
-  getNetworkBootstrapKey(web3RPC.shh, function(err, symKeyID){
+  getSymmetricKey(web3RPC.shh, function(err, symKeyID){
     if(err){console.log('ERROR:', err)}
     addSubscription(symKeyID, topics, web3RPC, onData) 
   })
+}
+
+function buildTopicString(topic) {
+  let hexString = '0x' + new Buffer(topic).toString('hex')
+  return hexString.substring(0, 10)
+}
+
+function buildFilterObject(topics) {
+  let hexTopics = []
+  for(let topic of topics){
+    hexTopics.push(buildTopicString(topic))
+  }
+  return {'topics': hexTopics}
+}
+
+// TODO: this can be improved to take in some defaults for ttl and workToProve
+// TODO: this can also perhaps have the option between an object with the parameters or 
+// the individual parameters
+function buildPostObject(shh, topic, payload, ttl, cb) {
+  getSymmetricKey(shh, function(err, symKeyID) {
+    if(err){console.log('ERROR:', err)}
+    getAsymmetricKey(shh, function(err, sig) {
+      if(err){console.log('ERROR:', err)}
+      postObj = { 
+        symkeyID,
+        sig,
+        topic,
+        payload,
+        ttl
+      };
+      cb(null, postObj);
+    });
+  });
+}
+
+// interval specified in milliseconds
+function postAtInterval(message, shh, topic, interval, cb) {  
+  let hexString = new Buffer(message).toString('hex')
+  buildPostObject(shh, topic, hexString, 10, function() {
+    let intervalID = setInterval(function(){
+      web3RPC.shh.post(postObj, function(err, res){
+        if(err){console.log('err', err)}
+      })
+    }, interval)
+    cb(null, intervalID);
+  });
 }
 
 exports.addSubscription = addSubscription
