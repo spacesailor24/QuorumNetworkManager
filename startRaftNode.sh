ab3940cc56e57b9ee15e7036556aa526037e8e89
Ryno Beyers <rynobey@gmail.com>
2018-02-28 08:04:19 +0200

    Added input parameter to raft shell script for targetgaslimit

--- startRaftNode.sh
+++ startRaftNode.sh
@@ -8,21 +8,21 @@ nohup constellation-node constellation.config &> constellation.log &
 
 sleep 5
 
-FLAGS="--datadir Blockchain --shh --port $1 --unlock 0 --password passwords.txt --raft"
+FLAGS="--datadir Blockchain --targetgaslimit $1 --shh --port $2 --unlock 0 --password passwords.txt --raft"
 
 RPC_API="admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft"
-HTTP_RPC_ARGS="--rpc --rpcaddr 0.0.0.0 --rpcport $2 --rpcapi $RPC_API"
-WS_RPC_ARGS="--ws --wsaddr 0.0.0.0 --wsport $3 --wsapi $RPC_API --wsorigins=*"
+HTTP_RPC_ARGS="--rpc --rpcaddr 0.0.0.0 --rpcport $3 --rpcapi $RPC_API"
+WS_RPC_ARGS="--ws --wsaddr 0.0.0.0 --wsport $4 --wsapi $RPC_API --wsorigins=*"
 
-RAFT_ARGS="--raftport $4"
+RAFT_ARGS="--raftport $5"
 
-if [ "$5" == "permissionedNodes" ]
+if [ "$6" == "permissionedNodes" ]
   then
   RAFT_ARGS="$RAFT_ARGS --permissioned Blockchain"
 fi
-if [ "$#" == 6 ]
+if [ "$#" == 7 ]
   then
-  RAFT_ARGS="$RAFT_ARGS --raftjoinexisting $6"
+  RAFT_ARGS="$RAFT_ARGS --raftjoinexisting $7"
 fi
 
 ALL_ARGS="$FLAGS $HTTP_RPC_ARGS $WS_RPC_ARGS $RAFT_ARGS"
